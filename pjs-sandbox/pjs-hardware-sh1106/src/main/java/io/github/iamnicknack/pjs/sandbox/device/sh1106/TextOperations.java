c6415bdd2731fc5c8da98a6c19645ac4f584b7e4
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2026-02-01 07:40:32 +0000

    (feature) always require position when drawing text

--- pjs-sandbox/pjs-hardware-sh1106/src/main/java/io/github/iamnicknack/pjs/sandbox/device/sh1106/TextOperations.java
+++ pjs-sandbox/pjs-hardware-sh1106/src/main/java/io/github/iamnicknack/pjs/sandbox/device/sh1106/TextOperations.java
@@ -2,6 +2,8 @@ package io.github.iamnicknack.pjs.sandbox.device.sh1106;
 
 import java.nio.ByteBuffer;
 
+import static io.github.iamnicknack.pjs.sandbox.device.sh1106.DisplayOperations.PAGE_SIZE;
+
 public interface TextOperations {
     /**
      * Write text to the display buffer.
@@ -9,13 +11,16 @@ public interface TextOperations {
      * @param column The column to write to.
      * @param text The text to write.
      */
-    void drawText(int page, int column, String text);
+    default void drawText(int page, int column, String text) {
+        drawText((page * PAGE_SIZE) + column, text);
+    }
 
     /**
-     * Write text to the display buffer at the current position.
+     * Write text to the display buffer at the specified position.
+     * @param position The position to write to.
      * @param text The text to write.
      */
-    void drawText(String text);
+    void drawText(int position, String text);;
 
     /**
      * Clear text from the display buffer.
@@ -23,13 +28,16 @@ public interface TextOperations {
      * @param column The column to start clearing from.
      * @param length The number of characters to clear.
      */
-    void clearText(int page, int column, int length);
+    default void clearText(int page, int column, int length) {
+        clearText((page * PAGE_SIZE) + column, length);
+    }
 
     /**
-     * Append a character to the display buffer at the current position.
-     * @param c The character to append.
+     * Clear text from the display buffer at the specified position.
+     * @param position The position to start clearing from.
+     * @param length The number of characters to clear.
      */
-    void appendChar(char c);
+    void clearText(int position, int length);
 
     static TextOperations create(DisplayOperations displayOperations) {
         return new Default(displayOperations);
@@ -43,30 +51,17 @@ public interface TextOperations {
         }
 
         @Override
-        public void drawText(int page, int column, String text) {
-            displayOperations.setPosition(page, column);
-            drawText(text);
-        }
-
-        @Override
-        public void drawText(String text) {
+        public void drawText(int position, String text) {
             var buffer = ByteBuffer.allocate(text.length() * 6);
             text.chars().forEach(c -> {
                 buffer.put(FontData.getCharacterData(c));
             });
-            displayOperations.setData(buffer.array(), 0, buffer.limit());
+            displayOperations.setData(position, buffer.array(), 0, buffer.limit());
         }
 
         @Override
-        public void clearText(int page, int column, int length) {
-            displayOperations.setPosition(page, column);
-            displayOperations.setData(new byte[length * 6], 0, length * 6);
-        }
-
-        @Override
-        public void appendChar(char c) {
-            displayOperations.setData(FontData.getCharacterData(c), 0, 6);
-
+        public void clearText(int position, int length) {
+            displayOperations.setData(position, new byte[length * 6], 0, length * 6);
         }
     }
 }
