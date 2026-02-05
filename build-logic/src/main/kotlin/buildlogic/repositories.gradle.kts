1d02b2be2ba1b2f6e534ca1f38ef712bbc72d6f2
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2026-02-05 08:00:04 +0000

    (chore) include selective snapshot registries

--- build-logic/src/main/kotlin/buildlogic/repositories.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic/repositories.gradle.kts
@@ -6,8 +6,8 @@ repositories {
 
     exclusiveContent {
         filter {
-            includeGroup("com.pi4j")
-        }
+            includeVersionByRegex("^com\\.pi4j$", "^pi4j-.*$", "^.*-SNAPSHOT$")
+            includeVersionByRegex("^io\\.github\\.iamnicknack$", "^pi4j-plugin-grpc$", "^.*-SNAPSHOT$")        }
         forRepository {
             maven {
                 url = uri("https://central.sonatype.com/repository/maven-snapshots/")
