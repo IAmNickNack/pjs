a5a1664ce879b0939a4899946ab0536bb344aebe
IAmNickNack <5727098+IAmNickNack@users.noreply.github.com>
2026-02-15 20:48:11 +0000

    feat: remove context dependency (#18)

--- providers/pjs-native/pjs-native-device/src/main/java/io/github/iamnicknack/pjs/ffm/NativeDeviceRegistryLoader.java
+++ providers/pjs-native/pjs-native-device/src/main/java/io/github/iamnicknack/pjs/ffm/NativeDeviceRegistryLoader.java
@@ -10,6 +10,7 @@ import io.github.iamnicknack.pjs.ffm.device.NativeSpiProvider;
 import io.github.iamnicknack.pjs.ffm.device.context.FileOperationsImpl;
 import io.github.iamnicknack.pjs.ffm.device.context.GpioOperationsImpl;
 import io.github.iamnicknack.pjs.ffm.device.context.IoctlOperationsImpl;
+import io.github.iamnicknack.pjs.ffm.device.context.PollingOperationsImpl;
 import io.github.iamnicknack.pjs.model.device.DeviceRegistry;
 import io.github.iamnicknack.pjs.model.device.DeviceRegistryLoader;
 import org.jspecify.annotations.Nullable;
@@ -39,14 +40,14 @@ public class NativeDeviceRegistryLoader implements DeviceRegistryLoader {
         var fileOperations = new FileOperationsImpl(context);
         var ioctlOperations = new IoctlOperationsImpl(context);
         var gpioOperations = new GpioOperationsImpl(fileOperations, ioctlOperations);
-//        var pollingOperations = new PollingOperationsImpl(context);
+        var pollingOperations = new PollingOperationsImpl(context);
 
         var i2cProvider = new NativeI2CProvider(fileOperations, ioctlOperations);
         var portProvider = new NativePortProvider(
                 gpioOperations.chipInfo("/dev/gpiochip0"),
-                context,
                 fileOperations,
-                ioctlOperations
+                ioctlOperations,
+                pollingOperations
         );
         var pwmProvider = new NativePwmProvider(fileOperations);
         var spiProvider = new NativeSpiProvider(
