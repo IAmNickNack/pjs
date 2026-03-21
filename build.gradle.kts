bd975b512a30d7cbac5ee1ef0bd4c1b63e17e392
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2026-03-21 08:23:56 +0000

    fix: update publish action

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,6 @@ subprojects {
 
 tasks.register("printVersion") {
     doLast {
-        withVersionCatalog { println("version: ${libs.versions.pjs.get()}") }
+        println("version: ${libs.versions.pjs.get()}")
     }
 }
