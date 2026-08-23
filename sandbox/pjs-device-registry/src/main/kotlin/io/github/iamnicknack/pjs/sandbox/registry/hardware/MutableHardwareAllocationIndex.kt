1d086ad55f1b2b1097d3b4078909124f1a5e4650
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2026-08-23 08:34:41 +0100

    track allocations in mutable index

--- sandbox/pjs-device-registry/src/main/kotlin/io/github/iamnicknack/pjs/sandbox/registry/hardware/MutableHardwareAllocationIndex.kt
+++ sandbox/pjs-device-registry/src/main/kotlin/io/github/iamnicknack/pjs/sandbox/registry/hardware/MutableHardwareAllocationIndex.kt
@@ -4,18 +4,39 @@ package io.github.iamnicknack.pjs.sandbox.registry.hardware
  * Mutable implementation of [HardwareAllocationIndex] which allows lines to be added at runtime
  */
 class MutableHardwareAllocationIndex(
-    private val lines: MutableSet<HardwareAllocationIndex.Line> = mutableSetOf()
+    private val lines: MutableSet<HardwareAllocationIndex.Line> = mutableSetOf(),
 ) : HardwareAllocationIndex.Mutable {
+
+    /**
+     * Tracker for in-use hardware allocations
+     */
+    private var inUse: HardwareAllocation = lines
+        .map { it.allocation }
+        .fold(HardwareAllocations.EMPTY) { acc, allocation -> acc or allocation }
+
+    /**
+     * Public accessor to the in-use hardware allocations tracker
+     */
+    val inUseAllocation: HardwareAllocation
+        get() = inUse
 
     constructor(vararg lines: HardwareAllocationIndex.Line) : this(lines.toSet().toMutableSet())
 
     override fun add(line: HardwareAllocationIndex.Line): HardwareAllocationIndex.Mutable {
+        require(inUse and line.allocation == HardwareAllocations.EMPTY) {
+            "Line intersects with existing lines"
+        }
         lines.add(line)
+        inUse = inUse or line.allocation
         return this
     }
 
     override fun remove(line: HardwareAllocationIndex.Line): HardwareAllocationIndex.Mutable {
+        require(lines.contains(line)) {
+            "Line does not exist"
+        }
         lines.remove(line)
+        inUse = inUse not line.allocation
         return this
     }
 
