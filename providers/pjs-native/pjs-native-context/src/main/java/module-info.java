837bfe3305f79c444c183813646f1cc98097a6d9
IAmNickNack <5727098+IAmNickNack@users.noreply.github.com>
2026-08-25 08:48:28 +0100

    chore: remove java module system (#76)

--- providers/pjs-native/pjs-native-context/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module pjs.ffm.context {
-    requires org.jspecify;
-    requires java.desktop;
-
-    exports io.github.iamnicknack.pjs.ffm.context;
-    exports io.github.iamnicknack.pjs.ffm.context.segment;
-    exports io.github.iamnicknack.pjs.ffm.context.method;
-}
