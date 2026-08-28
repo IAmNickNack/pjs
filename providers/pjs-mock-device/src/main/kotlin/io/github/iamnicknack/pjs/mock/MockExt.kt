12f1095a8db21d063eb68b236cc02d1f3e80af60
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2026-08-28 15:21:59 +0100

    Mock device interfaces

--- providers/pjs-mock-device/src/main/kotlin/io/github/iamnicknack/pjs/mock/MockExt.kt
+++ providers/pjs-mock-device/src/main/kotlin/io/github/iamnicknack/pjs/mock/MockExt.kt
@@ -10,9 +10,9 @@ val Spi.mockDelegate: MockSpi
         ?.delegate as? MockSpi
         ?: error("LoggingSpi is not the delegate for this Spi")
 
-val I2C.mockDelegate: MockI2C
+val I2C.mockDelegate: MockI2CImpl
     get() = (this as? LoggingI2C)
-        ?.delegate as? MockI2C
+        ?.delegate as? MockI2CImpl
         ?: error("LoggingI2C is not the delegate for this I2C")
 
 /**
